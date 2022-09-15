3eacbf601f0a0ad337d432424c5f700b2a0dab37
凌云！ <110074709+SuperLingyun@users.noreply.github.com>
2022-09-15 16:39:42 +0800

    Update qingyan.js

--- JS/qingyan.js
+++ JS/qingyan.js
@@ -14,10 +14,22 @@
 hostname =commerce-i18n-api.faceu.mobi,commerce-api.faceu.mobi
 
  */
-let obj = JSON.parse($response.body);
-obj.data.end_time=3725012184;
-obj.data.is_cancel_subscribe=false;
-obj.data.flag=true;
-$done({body: JSON.stringify(obj)});
+const path1 = "/commerce/v1/subscription/user_info";
+const path2 = "/apple/iosAppVerifyReceipt.php";
 
+let obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(path1) != -1){
+obj.data.start_time = 1584674770;
+obj.data.end_time = 4077660370;
+obj.data.is_cancel_subscribe = true;
+obj.data.flag = true;
+}
+if ($request.url.indexOf(path2) != -1){
+ obj.data = {
+    "isValid": 1,
+    "expiresTs": 4077660370
+}
+}
+$done({body: JSON.stringify(obj)});
 //轻颜相机
